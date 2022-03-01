a70b0f21432161a7fdd69c21830d439139c6fe65
zahhard <za.darardoust@gmail.com>
2022-03-01 20:08:06 +0330

    first initialized flag isCorrect added reset the color of buttons

--- app/src/main/java/com/example/mathplay/MainActivity.kt
+++ app/src/main/java/com/example/mathplay/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import com.example.mathplay.databinding.ActivityMainBinding
 import kotlin.random.Random
 import kotlin.random.nextInt
@@ -19,21 +20,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         buttonInit()
+        Game.nextLevel()
+        binding.tvRandom1.text = Game.a.toString()
+        binding.tvRandom2.text = Game.b.toString()
+        Game.randomOption()
+        setValueToButtons()
+
+
         binding.btnDice.setOnClickListener(::initView)
 
         binding.tvScore.text = Game.score.toString()
         for (i in 0..3){
             buttons[i].setOnClickListener {
+                var isCorrect = false
                 for (j in 0..3){
                     if (j == (Game.randomIndex)){
                         buttons[j].setBackgroundColor(Color.GREEN)
                         Game.score += 5
+                        isCorrect = true
                     }
                     else
                     {
                         buttons[j].setBackgroundColor(Color.RED)
-                        Game.score -= 2
                     }
+                }
+                if (!isCorrect){
+                    Game.score -= 2
                 }
                 binding.tvScore.text = Game.score.toString()
             }
@@ -42,10 +54,14 @@ class MainActivity : AppCompatActivity() {
 
     fun initView(view: View){
         Game.nextLevel()
+           
         binding.tvRandom1.text = Game.a.toString()
         binding.tvRandom2.text = Game.b.toString()
         Game.randomOption()
         setValueToButtons()
+        for (i in 0..3){
+            buttons[i].setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+        }
     }
 
     fun buttonInit(){
@@ -56,25 +72,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setValueToButtons(){
-        if (Game.randomIndex == 1){
+        if (Game.randomIndex == 0){
             buttons[0].text = Game.remainA.toString()
             buttons[1].text = Random.nextInt(1..100).toString()
             buttons[2].text = Random.nextInt(1..100).toString()
             buttons[3].text = Random.nextInt(1..100).toString()
         }
-        if (Game.randomIndex == 2){
+        if (Game.randomIndex == 1){
             buttons[1].text = Game.remainA.toString()
             buttons[0].text = Random.nextInt(1..100).toString()
             buttons[2].text = Random.nextInt(1..100).toString()
             buttons[3].text = Random.nextInt(1..100).toString()
         }
-        if (Game.randomIndex == 3){
+        if (Game.randomIndex == 2){
             buttons[2].text = Game.remainA.toString()
             buttons[1].text = Random.nextInt(1..100).toString()
             buttons[3].text = Random.nextInt(1..100).toString()
             buttons[0].text = Random.nextInt(1..100).toString()
         }
-        if (Game.randomIndex == 4){
+        if (Game.randomIndex == 3){
             buttons[3].text = Game.remainA.toString()
             buttons[1].text = Random.nextInt(1..100).toString()
             buttons[2].text = Random.nextInt(1..100).toString()
