13ef89b45903f73097de83a81063c01d3015939d
zahhard <za.darardoust@gmail.com>
2022-03-01 18:45:05 +0330

    an object called Game added to calc every thing about our game

--- app/src/main/java/com/example/mathplay/Game.kt
+++ app/src/main/java/com/example/mathplay/Game.kt
@@ -1,9 +1,31 @@
 package com.example.mathplay
 
-object Game {
-    var random1 = 0
-    var random2 = 0
-    var score = 0
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-    fun createRandoms(){}
+object Game {
+    var a = 0
+    var b = 0
+    var score = 0
+    var level = 1
+    var remainA = 0
+
+    fun createRandoms(){
+        a = Random.nextInt(1..100)
+        b = Random.nextInt(1..10)
+    }
+
+    fun nextLevel(){
+        createRandoms()
+        level ++
+        calcRemain()
+    }
+
+    fun calcRemain (){
+        if (a > b){
+            remainA = a % b
+        }
+        else
+            remainA = b % a
+    }
 }
