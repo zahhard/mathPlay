c12cccc6244f0546c57a4c74b95133c324cecc65
zahhard <za.darardoust@gmail.com>
2022-03-02 01:10:56 +0330

    operator checked

--- app/src/main/java/com/example/mathplay/Game.kt
+++ app/src/main/java/com/example/mathplay/Game.kt
@@ -8,32 +8,53 @@ object Game {
     var b = 0
     var score = 0
     var level = 0
-    var remainA = 0
+    var result = 0
     var randomIndex = 0
-    var startRangA = 0
-    var endRangA = 100
-    var startRangB = 0
-    var endRangB = 10
+    var startRangA = "0"
+    var endRangA = "100"
+    var startRangB = "0"
+    var endRangB = "10"
     var operator = ""
 
 
     fun createRandoms(){
-        a = Random.nextInt(startRangA..endRangA)
-        b = Random.nextInt(startRangB..endRangB)
+        a = Random.nextInt(startRangA.toInt()..endRangA.toInt())
+        b = Random.nextInt(startRangB.toInt()..endRangB.toInt())
     }
 
     fun nextLevel(){
         createRandoms()
         level ++
-        calcRemain()
+
+        calcResult()
     }
 
-    fun calcRemain (){
-        if (a > b){
-            remainA = a % b
+    fun calcResult (){
+        if (operator == "%") {
+            if (a > b) {
+                result = a % b
+            } else
+                result = b % a
         }
-        else
-            remainA = b % a
+
+        if (operator == "*"){
+            result = a * b
+        }
+        if (operator == "+"){
+            result = a + b
+        }
+        if (operator == "-"){
+            if (a > b) {
+                result = a - b
+            } else
+                result = b - a
+        }
+        if (operator == "/"){
+            if (a > b) {
+                result = a / b
+            } else
+                result = b / a
+        }
     }
 
     fun randomOption(){
