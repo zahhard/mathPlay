c75bbad78748a0875ceda72783249b304da07c6b
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-03 03:10:07 +0330

    checked operator field not to be empty in GameSettingActivity.kt

--- app/src/main/java/com/example/mathplay/GameSettingActivity.kt
+++ app/src/main/java/com/example/mathplay/GameSettingActivity.kt
@@ -31,43 +31,49 @@ class GameSettingActivity : AppCompatActivity() {
                 binding.endRangeB.text.isNullOrBlank() &&
                 binding.edChooseOperator.text.isNullOrBlank()
             ) {
-                Toast.makeText(this, " At least please fill the ranges", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "please fill all fields", Toast.LENGTH_SHORT).show()
             } else {
                 val inputOperator = binding.edChooseOperator.text
-                if (inputOperator.isNullOrBlank()) {
-                    Game.operator = "%"
-                } else if (inputOperator.matches(Regex("^[*]|[-]|[+]|[/]|[%]$"))) {
+                if (inputOperator.isNullOrBlank() || !inputOperator.matches(Regex("^[*]|[-]|[+]|[/]|[%]$"))) {
+                    Toast.makeText(this, " insert an oprator between /*%+-", Toast.LENGTH_SHORT).show()
+                } else {
                     Game.operator = inputOperator.toString()
-                }
-                try {
-                    if (binding.startRangeA.text.isNullOrBlank()||
-                         binding.endRangeA.text.isNullOrBlank()||
-                            binding.startRangeB.text.isNullOrBlank()||
-                        binding.startRangeB.text.isNullOrBlank())
-                    {
-                        Toast.makeText(this, "please fill all the ranges", Toast.LENGTH_SHORT).show()
-                    }
-                       else {
-                        if (binding.startRangeA.text.toString().toInt() < binding.endRangeA.text.toString().toInt() && !binding.startRangeA.text.isNullOrBlank() && !binding.endRangeA.text.isNullOrBlank()) {
-                            Game.startRangA = binding.startRangeA.text.toString()
-                            Game.endRangA = binding.endRangeA.text.toString()
-                            isSetA = true
+                    try {
+                        if (binding.startRangeA.text.isNullOrBlank() ||
+                            binding.endRangeA.text.isNullOrBlank() ||
+                            binding.startRangeB.text.isNullOrBlank() ||
+                            binding.startRangeB.text.isNullOrBlank()
+                        ) {
+                            Toast.makeText(this, "please fill all the ranges", Toast.LENGTH_SHORT)
+                                .show()
                         } else {
-                            binding.startRangeA.error = "inValid range"
-                            binding.endRangeA.error = "inValid range"
-                        }
+                            if (binding.startRangeA.text.toString()
+                                    .toInt() < binding.endRangeA.text.toString()
+                                    .toInt() && !binding.startRangeA.text.isNullOrBlank() && !binding.endRangeA.text.isNullOrBlank()
+                            ) {
+                                Game.startRangA = binding.startRangeA.text.toString()
+                                Game.endRangA = binding.endRangeA.text.toString()
+                                isSetA = true
+                            } else {
+                                binding.startRangeA.error = "inValid range"
+                                binding.endRangeA.error = "inValid range"
+                            }
 
-                        if (binding.startRangeB.text.toString().toInt() < binding.endRangeB.text.toString().toInt() && !binding.startRangeB.text.isNullOrBlank() && !binding.endRangeB.text.isNullOrBlank()) {
-                            Game.startRangB = binding.startRangeB.text.toString()
-                            Game.endRangB = binding.endRangeB.text.toString()
-                            isSetB = true
-                        } else {
-                            binding.startRangeB.error = "inValid range"
-                            binding.endRangeB.error = "inValid range"
+                            if (binding.startRangeB.text.toString()
+                                    .toInt() < binding.endRangeB.text.toString()
+                                    .toInt() && !binding.startRangeB.text.isNullOrBlank() && !binding.endRangeB.text.isNullOrBlank()
+                            ) {
+                                Game.startRangB = binding.startRangeB.text.toString()
+                                Game.endRangB = binding.endRangeB.text.toString()
+                                isSetB = true
+                            } else {
+                                binding.startRangeB.error = "inValid range"
+                                binding.endRangeB.error = "inValid range"
+                            }
                         }
+                    } catch (e: Exception) {
+                        Toast.makeText(this, "invalid", Toast.LENGTH_SHORT).show()
                     }
-                } catch (e: Exception) {
-                    Toast.makeText(this, "invalid", Toast.LENGTH_SHORT).show()
                 }
 
                 if (isSetA && isSetB) {
